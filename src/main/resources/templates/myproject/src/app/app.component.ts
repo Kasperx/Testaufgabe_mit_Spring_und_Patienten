8ab34f67e29984aecf836342c92bcfaa74350b0d
Kasperix <titango2011@gmail.com>
2024-04-28 15:15:50 +0200

    angular with bootstrap

--- src/main/resources/templates/myproject/src/app/app.component.ts
+++ src/main/resources/templates/myproject/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { PersonListComponent } from './person-list/person-list.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    PersonListComponent
+    PersonListComponent,
+    NgbModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
